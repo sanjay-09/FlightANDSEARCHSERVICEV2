cecf3553d664f55215ff986f7d02beb4819a8e0b
Sanjay Singh <sanjay.s01558@gmail.com>
2025-07-18 21:42:02 +0530

    added city model and migrations

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express=require("express");
+const bodyParser=require("body-parser");
 
 const {PORT}=require("./config/serverConfig.js")
 
-const setUpAndStartServer=function(){
+const setUpAndStartServer=async function(){
 
     const app=express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+
 
     app.listen(PORT,function(){
         console.log(`listening on the PORT ${PORT}`);
