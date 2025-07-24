2cf62a9b243d0c0ed4829637f9902e82070455ee
Sanjay Singh <sanjay.s01558@gmail.com>
2025-07-24 17:02:29 +0530

    final changes done

--- src/repository/seat-repository.js
+++ src/repository/seat-repository.js
@@ -1,92 +1,61 @@
-const {sequelize}=require("../models/index");
-const {Seat}=require("../models/index");
-class SeatRepository{
+const { sequelize } = require("../models/index");
+const { Seat } = require("../models/index");
+class SeatRepository {
+  async lockRows(data) {
+    const { flight_id, totalSeats } = data;
+    const transaction = await sequelize.transaction();
 
-    async lockRows(data){
-         const { flight_id, totalSeats } = data;
-         const transaction = await sequelize.transaction();
+    try {
+      const seats = await Seat.findAll({
+        where: {
+          flight_id,
+          status: "AVAILABLE",
+        },
+        limit: parseInt(totalSeats),
+        lock: transaction.LOCK.UPDATE,
+        transaction,
+      });
+      console.log(seats);
 
-      try {
-       const seats = await Seat.findAll({
-      where: {
-        flight_id,
-        status: 'AVAILABLE'
-      },
-      limit:parseInt(totalSeats),
-      lock: transaction.LOCK.UPDATE,
-      transaction
-    });
-    console.log(seats)
-  
+      if (seats.length < totalSeats) {
+        throw new Error("Not enough seats");
+      }
 
+      const seatIds = [];
+      for (const seat of seats) {
+        seat.status = "LOCKED";
+        await seat.save({ transaction });
+        seatIds.push(seat.id);
+      }
 
-    if (seats.length < totalSeats) {
-      throw new Error('Not enough seats');
-    }
+      await transaction.commit();
 
-    const seatIds = [];
-    for (const seat of seats) {
-      seat.status = 'LOCKED';
-      await seat.save({  transaction });
-      seatIds.push(seat.id);
-    }
+      return seatIds;
+    } catch (err) {
+      console.log(err);
 
-    await transaction.commit();
-
-
-    return seatIds;
-  } 
-  catch(err){
-    console.log(err);
-  
       await transaction.rollback();
-     throw err;
+      throw err;
+    }
+  }
+  async confirmSeats(data) {
+    try {
+      const { seatIds } = data;
 
+      await Seat.update({ status: "BOOKED" }, { where: { id: seatIds } });
+    } catch (err) {
+      throw err;
+    }
   }
 
+  async releaseSeats(data) {
+    try {
+      const { seatIds } = data;
 
-
+      await Seat.update({ status: "AVAILABLE" }, { where: { id: seatIds } });
+    } catch (err) {
+      throw err;
     }
-    async confirmSeats(data){
-      try{
-
-        const {seatIds}=data;
-
-         await Seat.update(
-    { status: 'BOOKED' },
-    { where: { id: seatIds } }
-  );
-
+  }
 }
-  
-       
-       catch(err){
-        throw err;
-        
-       }
-
-    }
-
-    async releaseSeats(data){
-      try{
-
-        const {seatIds}=data;
-
-         await Seat.update(
-    { status: 'AVAILABLE' },
-    { where: { id: seatIds } }
-  );
-
-}
-       catch(err){
-        throw err;
-        
-       }
-
-    }
-
-
-    
-
-}
-module.exports=SeatRepository;
+module.exports = SeatRepository;
